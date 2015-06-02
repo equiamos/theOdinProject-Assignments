0973c826edb465868e9ff9d5f885f64dbb98c389
Morgan Owens <mowensequestrian@gmail.com>
2015-06-02 19:57:12 -0400

    Made tests pass. [-repeater]

--- ruby/rspec-intro/05_silly_blocks/silly_blocks.rb
+++ ruby/rspec-intro/05_silly_blocks/silly_blocks.rb
@@ -1,3 +1,9 @@
 def reverser(string)
-  string.reverse!
+  array = string.split(" ")
+  array.map { |word| word.reverse! }*" "
+  # *" " converts back to string
+end
+
+def adder(x, add=1)
+  x + add
 end
