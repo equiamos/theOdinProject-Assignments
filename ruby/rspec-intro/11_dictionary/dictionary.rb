95e5d4a1031b4ff599cb1981e209fcca0a508515
Morgan Owens <mowensequestrian@gmail.com>
2015-06-03 19:30:55 -0400

    Fix tests to make work with official soln

---
+++ ruby/rspec-intro/11_dictionary/dictionary.rb
@@ -0,0 +1,35 @@
+
+# BELOW Solution from official solutions used to fix tests:
+# class Dictionary
+#   def entries
+#     @entries ||= {}
+#   end
+#
+#   def add(key, value=nil)
+#     entries[key] = value
+#   end
+#
+#   def keywords
+#     @entries.keys.sort
+#   end
+#
+#   def include? word
+#     entries.keys.include? word
+#   end
+#
+#   def find partial_word
+#     result = {}
+#     entries.each_pair do |key, definition|
+#       if key =~ /^#{partial_word}/
+#         result[key] = definition
+#       end
+#     end
+#     result
+#   end
+#
+#   def printable
+#     entries.map do |key_val|
+#       %Q{[#{key_val.first}] "#{key_val.last}"}
+#     end.sort.join("\n")
+#   end
+# end
