e67c93386ead1327e85e326b928a8bc3ee97a3af
Morgan Owens <mowensequestrian@gmail.com>
2015-06-03 13:25:32 -0400

    Add empty method/class

--- ruby/rspec-intro/05_silly_blocks/silly_blocks.rb
+++ ruby/rspec-intro/05_silly_blocks/silly_blocks.rb
@@ -2,11 +2,15 @@ def reverser(string)
 #  array = string.split(" ")
 #  array.map { |word| word.reverse! }*" "
   #*" " converts back to string
-  
+
   string.gsub(/\S+/, &:reverse)
   # Appears to work same as above code
 end
 
 def adder(x, add=1)
   x + add
+end
+
+def repeater(num=nil)
+
 end
